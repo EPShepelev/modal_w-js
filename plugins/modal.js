4eeb429203b0345ee2ed47d30e576274d9e9a85f
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2020-05-02 15:15:08 +0300

    create function  _createModal and methods open, close for modal

--- plugins/modal.js
+++ plugins/modal.js
@@ -1,3 +1,38 @@
-$.modal = function() {
-  
+function _createModal(options) {
+  const modal = document.createElement('div')
+  modal.classList.add('ourmodal')
+  modal.insertAdjacentHTML('afterbegin', `
+<div class="modal-overlay">
+      <div class="modal-window">
+        <div class="modal-header">
+          <span class="modal-title">Awesome Title</span>
+          <span class="modal-close">&times;</span>
+        </div>
+        <div class="modal-body">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, libero?</p>
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+        </div>
+        <div class="modal-footer">
+          <button>Accept</button>
+          <button>Cancel</button>
+        </div>
+      </div>
+    </div>
+    `)
+    document.body.appendChild(modal)
+  return modal
+}
+
+$.modal = function(options) {
+  $modal = _createModal(options)
+
+  return {
+    open() {
+      $modal.classList.add('open')
+    },
+    close() {
+      $modal.classList.remove('open')
+    },
+    destroy() {}
+  }
 }
