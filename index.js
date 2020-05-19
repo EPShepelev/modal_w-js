f639f0911a28d58fceb79c52c9272e9a3e4e81e9
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2020-05-19 16:31:47 +0300

    create options object for modal window

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const myModal = $.modal()
+const myModal = $.modal({
+  //передача базовых опций для модального окна
+  title: 'Modal Window',
+  closable: true,
+  content: `
+  <h3>Modal window is working</h3>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit in magnam dignissimos, harum</p>
+  `,
+  width: '400px'
+})
