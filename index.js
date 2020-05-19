0c370bd8028f95fddcbfa6daec0f3b46ee67754b
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2020-05-19 18:28:15 +0300

    create footer options and handler for footer-buttons

--- index.js
+++ index.js
@@ -6,5 +6,23 @@ const myModal = $.modal({
   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit in magnam dignissimos, harum</p>
   <p>Lorem ipsum, dolor sit amn magnam dignissimos, harum</p>
   `,
-  width: '400px'
+  width: '400px',
+  footerButtons: [
+    {
+      text: 'OK',
+      type: 'primary',
+      handler() {
+        console.log('Primary btn clicked')
+        myModal.close()
+      }
+    },
+    {
+      text: 'Cancel',
+      type: 'danger',
+      handler() {
+        console.log('Danger btn clicked')
+        myModal.close()
+      }
+    }
+  ]
 })
