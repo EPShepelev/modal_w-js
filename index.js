6916cfee0e10efcea29f6699f84cc579130580c9
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2020-05-21 19:01:13 +0300

    added massive cars

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const cars = [
+  {id: 1, title: 'Dodge', price: 25, img: 'https://avatars.mds.yandex.net/get-pdb/1615223/1e089d46-25b9-48b6-a522-c7d5b9716b34/s1200?webp=false'},
+  {id: 2, title: 'Aston Martin', price: 110, img: 'https://avatars.mds.yandex.net/get-pdb/1615223/1e089d46-25b9-48b6-a522-c7d5b9716b34/s1200?webp=false'},
+  {id: 3, title: 'Jaguar', price: 80, img: 'https://avatars.mds.yandex.net/get-pdb/1615223/1e089d46-25b9-48b6-a522-c7d5b9716b34/s1200?webp=false'}
+]
+
 const myModal = $.modal({
   //передача базовых опций для модального окна
   title: 'Modal Window',
